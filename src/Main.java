8df3cb37b056edd5787202a2ac5a02e8ac32cb7c
serkanakgok <serkan.akgok@hotmail.com>
2024-01-15 18:30:33 +0300

    palidrom sayi bulma

--- src/Main.java
+++ src/Main.java
@@ -5,25 +5,18 @@ public class Main {
     static boolean isPalindrom(int number) {
         int temp = number, reverseNumber = 0, lastNumber;
         while (temp != 0) {
-            //System.out.println("===========");
-            //System.out.println("Sayı => " + temp);
-
             lastNumber = temp % 10;
-            //System.out.println("Son basamak =>" + lastNumber);
-
             reverseNumber = (reverseNumber * 10) + lastNumber;
-            //System.out.println("Yeni sayı =>" + reverseNumber);
             temp /= 10;
         }
         if (number == reverseNumber)
             return true;
         else
-            return  false;
+            return false;
     }
 
     public static void main(String[] args) {
         System.out.println(isPalindrom(2442));
-
     }
 
 
